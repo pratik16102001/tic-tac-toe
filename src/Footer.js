bd2c5022e5194918a135b72647ce3b242a48ee34
pratik <pratikvai.blackandwhiteinfotech@gmail.com>
2024-05-14 22:00:26 +0530

    initial commit

---
+++ src/Footer.js
@@ -0,0 +1,12 @@
+const Footer = () => {
+    return (
+        <footer className="sticky bottom-0 py-6 bg-zinc-50">
+            <div className="max-w-7xl mx-auto px-3">
+                <div className="flex justify-center items-center">
+                </div>
+            </div>
+        </footer>
+    )
+}
+
+export default Footer
